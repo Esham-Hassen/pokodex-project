3051be5673571d3b0744120ad726b6af58eb7499
Esham-Hassen <eshamhassen31@gmail.com>
2024-06-06 23:04:53 +0200

    added type colors

--- pokodex-project/script.js
+++ pokodex-project/script.js
@@ -1,6 +1,7 @@
-
-let currentPokemons = 20;
+let currentPokemons = 40;
 let allPokemons = [];
+
+
 
 function init() {
   fetchPokemon();
@@ -34,17 +35,24 @@ async function loadPokemonDetails(pokemonList) {
 async function renderAllPokemonCards() {
   const container = document.getElementById("pokemon-container");
   container.innerHTML = "";
-
-  for (let i = 0; i < allPokemons.length; i++) {
+    for (let i = 0; i < allPokemons.length; i++) {
     const pokemon = allPokemons[i];
     const capitalizedPokemonName = capitalizeFirstLetter(pokemon.name);
     const image = getPokemonImage(pokemon);
-  
-    container.innerHTML += `<div class="pokemon-card">
-    <h2>${capitalizedPokemonName}</h2>
-    <img src="${image}">
-  </div>`;
+    const typeColor = getTypeColor(pokemon);
+    container.innerHTML += generatePokemonCardsHTML(image,typeColor,capitalizedPokemonName);
   }
+}
+
+function generatePokemonCardsHTML(image, typeColor, capitalizedPokemonName) {
+  const container = document.getElementById("pokemon-container");
+  container.innerHTML = "";
+ return container.innerHTML += `
+<div class="pokemon-card" style="background-color:${typeColor}">
+  <h2>${capitalizedPokemonName}</h2>
+  <img src="${image}">
+</div>
+`;
 }
 
 function capitalizeFirstLetter(string) {
@@ -52,5 +60,19 @@ function capitalizeFirstLetter(string) {
 }
 
 function getPokemonImage(pokemon) {
-return pokemon.sprites.front_default
+  return pokemon.sprites.front_default;
 }
+
+function getTypeColor(pokemon) {
+  const primaryType = pokemon.types[0].type.name;
+  return typeColors[primaryType] || "#FFFFFF";
+}
+
+
+const typeColors = { 
+  normal: "#A8A878", fire: "#FF7D25", water: "#6390F0", electric: "#FFD700", 
+  grass: "#78C850", ice: "#98D8D8", fighting: "#C03028", poison: "#A040A0", 
+  ground: "#E0C068", flying: "#A890F0", psychic: "#F85888", bug: "#A8B820", 
+  rock: "#B8A038", ghost: "#705898", dragon: "#7038F8", dark: "#705848", 
+  steel: "#B8B8D0", fairy: "#EE99AC" 
+};
